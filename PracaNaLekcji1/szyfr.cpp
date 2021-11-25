4e084e0c0c1f8195e7e4b48b9e54cadb053eccab
MasterSun8 <kacpi.doro@gmail.com>
2021-11-25 20:13:42 +0100

    cesar

--- PracaNaLekcji1/szyfr.cpp
+++ PracaNaLekcji1/szyfr.cpp
@@ -2,8 +2,33 @@
 #include <string>
 using namespace std;
 
+string cesar(string a, int offset=0){
+    string o = "";
+    int x;
+    if(offset>25 && offset<0){
+        return "Offset is too big.";
+    }
+    for(char& b : a){
+        x = int(b) + offset;
+        b = x % ('z' + 1);
+        if(b > 'a'-1){
+            o += b;
+        }else{
+            o += b + 'a';
+        }
+    }
+    return o;
+}
+
 int main(){
     string h;
-    cin >> h;
-    cout << h;
+    int y;
+    cout << "Password: ";
+    getline(cin, h);
+    
+    cout << "Offset: ";
+    
+    cin >> y;
+    
+    cout << cesar(h, y) << endl;
 }
